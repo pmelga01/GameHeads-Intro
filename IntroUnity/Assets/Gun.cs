dacabd6722b5427ab6d5395ed1c9fe9e20b3525c
Pamela Melgar <pamela.melgar@tufts.edu>
2024-05-11 16:24:22 -0400

    Press F to fire bullets from player facing
    speedy

--- IntroUnity/Assets/Gun.cs
+++ IntroUnity/Assets/Gun.cs
@@ -5,16 +5,29 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     public GameObject bulletPrefab;
+    public GameObject playerAgent;
+
+    private float speed = 300.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerAgent = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        GameObject.Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        if (Input.GetKeyDown(KeyCode.F)) {
+            PlayerShoot();
+        }
+        
+    }
+
+    void PlayerShoot()
+    {
+        GameObject spawnBullet = GameObject.Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        Rigidbody rb = spawnBullet.GetComponent<Rigidbody>();
+        rb.AddForce(playerAgent.transform.forward * speed);
     }
 }
